e999cdfc4104066fc2d44bb051df14dd57a698e4
KimSeungRyul <seunglul1999@gmail.com>
2025-12-13 13:50:03 +0900

    chore: spring boot version 조정
    - spring boot 4.0 도입으로 인한 기존 플러그인 충돌 해결을 위한 버전 조정

--- moyoy-api/build.gradle.kts
+++ moyoy-api/build.gradle.kts
@@ -1,7 +1,39 @@
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
 import org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask
 
 tasks.getByName("bootJar") {
     enabled = true
+}
+
+plugins {
+    id("org.asciidoctor.jvm.convert") version "4.0.0"
+}
+
+configurations {
+    create("asciidoctorExt")
+}
+
+dependencies {
+    implementation(project(":moyoy-common"))
+    implementation(project(":moyoy-core:domain"))
+    implementation(project(":moyoy-core:infra"))
+
+    "asciidoctorExt"("org.springframework.restdocs:spring-restdocs-asciidoctor:4.0.0")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:4.0.0")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+val snippetsDir by extra { file("build/generated-snippets") }
+
+tasks.named<Test>("test") {
+
+    outputs.dir(snippetsDir)
+}
+
+tasks.named<AsciidoctorTask>("asciidoctor") {
+    inputs.dir(snippetsDir)
+    configurations("asciidoctorExt")
+    dependsOn(tasks.named("test"))
 }
 
 afterEvaluate {
@@ -17,10 +49,4 @@ afterEvaluate {
             )
         }
     }
-}
-
-dependencies {
-    implementation(project(":moyoy-common"))
-    implementation(project(":moyoy-core:domain"))
-    implementation(project(":moyoy-core:infra"))
 }
