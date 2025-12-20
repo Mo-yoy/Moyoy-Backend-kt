9835683aa696af8826bb6fbe999d177562fff350
KimSeungRyul <seunglul1999@gmail.com>
2025-12-20 13:41:01 +0900

    chore: domain 모듈 jpa 플러그인 추가

--- moyoy-core/domain/build.gradle.kts
+++ moyoy-core/domain/build.gradle.kts
@@ -1,6 +1,11 @@
 tasks.getByName("jar") {
     enabled = true
 }
+
+plugins {
+    kotlin("plugin.jpa")
+}
+
 dependencies {
     implementation(project(":moyoy-common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
