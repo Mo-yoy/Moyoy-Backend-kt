e0de5952ce985e0d8ffe686d7ecd03fa3db8e2d9
KimSeungRyul <seunglul1999@gmail.com>
2025-12-23 21:24:26 +0900

    chore: h2 테스트 db 추가

--- moyoy-core/domain/build.gradle.kts
+++ moyoy-core/domain/build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
 dependencies {
     implementation(project(":moyoy-common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("com.h2database:h2")
 }
