850f732e93b4334d5381a8878fc42efa32837c2d
KimSeungRyul <seunglul1999@gmail.com>
2025-12-23 20:51:45 +0900

    chore: xxxProperties 테스트 커버리지에서 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,12 @@ subprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+
+        testLogging {
+            events("passed", "skipped", "failed", "standardOut", "standardError")
+            showStandardStreams = true
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
         finalizedBy(tasks.named<JacocoReport>("jacocoTestReport"))
     }
 
@@ -81,6 +87,7 @@ subprojects {
                         exclude(
                             "**/*Application*",
                             "**/*Config*",
+                            "**/*Properties*",
                             "**/*Dto*",
                             "**/*Request*",
                             "**/*Response*",
@@ -120,6 +127,7 @@ sonar {
                 "**/resources/**",
                 "**/*Application*",
                 "**/*Config*",
+                "**/*Properties*",
                 "**/*Dto*",
                 "**/*Request*",
                 "**/*Response*",
