9ed9d833d64068f6a60b05744261f5c5eaba30a7
KimSeungRyul <seunglul1999@gmail.com>
2026-01-01 02:20:21 +0900

    feat: GithubErrorDecoder 구현

--- moyoy-core/infra/src/main/kotlin/com/moyoy/infra/feign/github/GithubErrorDecoder.kt
+++ moyoy-core/infra/src/main/kotlin/com/moyoy/infra/feign/github/GithubErrorDecoder.kt
@@ -10,6 +10,10 @@ import com.moyoy.infra.feign.github.error.GithubValidationFailedException
 import feign.Response
 import feign.codec.ErrorDecoder
 
+/**
+ * 깃허브에서 주는 ErrorMessage가 의미 있는 메시지인지 의문이라 status 위주로만 만들까, message 까지 활용해 볼까 아직 고민중
+ * 3xx 응답이 필요한 경우는 아직 없었는데 필요할 경우 여기서 제어
+ */
 class GithubErrorDecoder : ErrorDecoder {
     override fun decode(
         methodKey: String,
